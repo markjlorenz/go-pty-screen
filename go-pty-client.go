59ce610547611bcb44d743dc84d8cadcf3703cd0
Mark Lorenz <markjlorenz@dapplebeforedawn.com>
2013-12-10 23:46:56 -0500

    forked it, read the termios man page, fixed it.
    fuck yea, opensource.

--- go-pty-client.go
+++ go-pty-client.go
@@ -6,6 +6,7 @@ import (
   "os"
   "net"
   "dapplebeforedawn/share-pty/options"
+  "github.com/dapplebeforedawn/go.crypto/ssh/terminal"
 )
 
 func main() {
@@ -18,8 +19,13 @@ func main() {
   screen_conn, err  := net.Dial("tcp", opts.ServerIP+":"+opts.ScreenPort)
   if err != nil { panic(err) }
 
+  tty, _       := os.Open("/dev/tty")
+  tty_fd       := int( tty.Fd() )
+  tty_mode, _  := terminal.MakeRaw( tty_fd )
+
   go io.Copy(key_conn, os.Stdin)
   io.Copy(os.Stdout, screen_conn)
 
   fmt.Println("type 'reset' to restore your terminal")
+  terminal.Restore( tty_fd, tty_mode )  // why u no work?
 }
