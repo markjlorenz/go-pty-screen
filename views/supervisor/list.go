6d6deea0c7e87d621a03981cad3602c3cdcce1e5
Mark Lorenz <markjlorenz@dapplebeforedawn.com>
2013-12-15 13:17:05 -0500

    good looking rows

--- views/supervisor/list.go
+++ views/supervisor/list.go
@@ -3,6 +3,9 @@ package supervisor_views
 import (
   "code.google.com/p/goncurses"
   "dapplebeforedawn/share-pty/servers"
+  "strings"
+  "strconv"
+  "fmt"
 )
 
 type List struct {
@@ -16,15 +19,30 @@ func NewList() (list *List){
   if err != nil { panic(err) }
 
   list.Window = &window
-  list.Move(1, 1)
+  list.MovePrintln(1, 2, list.build_row("ALIAS", "COMMAND", "KEY_PORT", "SCREEN_PORT"))
+  list.MovePrintln(2, 2, "No servers running.  Type: `new <alias> <command> <rows> <cols>` into the command window to start one.")
+  list.Move(2, 2)
   list.Border()
   return
 }
 
 func (list *List) AddItem(item pty_servers.PtyShare) (){
-  lasty, _ := list.Getyx()
-  list.MovePrintln(lasty, 2, item.Alias)
+  lasty, _    := list.Getyx()
+
+  key_port    := strconv.Itoa(item.KeyServer.Port)
+  screen_port := strconv.Itoa(item.ScreenServer.Port)
+  list.MovePrintln(lasty, 2, list.build_row(item.Alias, item.Command, key_port, screen_port))
+
   list.Border()
+}
+
+func (list *List) build_row(alias, command, key_port, screen_port string) (string){
+  _, row_length := list.Maxyx()
+  field_count   := 5 // 5 fields in PtyShare
+  segment_size  := row_length / field_count
+  format_string := strings.Repeat("%-"+strconv.Itoa(segment_size)+"s", field_count)
+
+  return fmt.Sprintf(format_string, alias, command, key_port, screen_port, "")
 }
 
 func (list *List) Border() {
