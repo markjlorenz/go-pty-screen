b1544dbdef4fe1f9bd0f384111a14b1da607b1e8
Mark Lorenz <markjlorenz@dapplebeforedawn.com>
2013-12-25 23:19:22 -0500

    open, don't create

--- options/tunnel.go
+++ options/tunnel.go
@@ -15,7 +15,7 @@ func (c *Tunnel) Parse() {
   port := "2001"
 
   ip_filename  := "/tmp/go-pty-tunnel~ip"
-  ip_file, _   := os.Create(ip_filename)
+  ip_file, _   := os.Open(ip_filename)
   server_ip, _ := ioutil.ReadAll(ip_file)
 
   flag.StringVar(&c.Port, "port", port, "the tunnel port")
