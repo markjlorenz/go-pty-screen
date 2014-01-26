b3019cb91685df54836786db2d2604a5e17844f0
Mark Lorenz <markjlorenz@dapplebeforedawn.com>
2014-01-25 19:54:54 -0500

    code cleanup

--- zeroconf/client.go
+++ zeroconf/client.go
@@ -40,37 +40,8 @@ func (c *Client) Dial() {
     go dnssd.Process(rctx)
     resolveReply, _ := <-rc
 
-    // qc := make(chan *dnssd.QueryRecordReply)
-    // qctx, err := dnssd.QueryRecord(
-    //   dnssd.DNSServiceFlagsForceMulticast,
-    //   resolveReply.InterfaceIndex,
-    //   resolveReply.FullName,
-    //   dnssd.DNSServiceType_SRV,
-    //   dnssd.DNSServiceClass_IN,
-    //   qc,
-    // )
-    // if err != nil { println(err); return }
-
-    // go dnssd.Process(qctx)
-    // queryRecordReply, _ := <-qc
-
-    // gc := make(chan *dnssd.GetAddrInfoReply)
-    // gctx, err := dnssd.GetAddrInfo(
-    //   dnssd.DNSServiceFlagsForceMulticast,
-    //   0,
-    //   dnssd.DNSServiceProtocol_IPv4,
-    //   resolveReply.HostTarget,
-    //   gc,
-    // )
-    // if err != nil { println(err); return }
-
-    // go dnssd.Process(gctx)
-    // getAddrInfoReply, _ := <-gc
-
-    // c.Host = getAddrInfoReply.Ip
     c.Host = resolveReply.HostTarget
     c.Port = int(resolveReply.Port)
     break
   }
-
 }
--- zeroconf/server.go
+++ zeroconf/server.go
@@ -13,30 +13,10 @@ func StartAnnounce(port int) {
     "_goptyscreen._tcp.",
     "",
     "",
-    2000,
+    port,
     rc,
   )
   if err != nil { println(err); return }
 
   go dnssd.Process(ctx)
 }
-
-// package zeroconf
-//
-// import (
-//   "os/exec"
-//   "strconv"
-// )
-//
-// type Server struct {
-//   zeroconf *exec.Cmd
-// }
-//
-// func StartAnnounce(port int) {
-//   // Start mDNS/zeroconf/Bonjour/Avahi, whatever it's called
-//   zeroconf := exec.Command("dns-sd", "-R", "GoPtyScreen", "_goptyscreen._tcp", ".", strconv.Itoa(port))
-//   defer zeroconf.Process.Kill()
-//
-//   zeroconf.Start()
-//   zeroconf.Wait()
-// }
